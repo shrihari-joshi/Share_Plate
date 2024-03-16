59bd71ef3f29aaf6cfe5654fb9b99d7603bab954
AK-47Code <atharvakhabale2004@gmail.com>
2024-03-16 18:44:39 +0530

    Some changes

--- app/src/Components/Homepage/Homepage.jsx
+++ app/src/Components/Homepage/Homepage.jsx
@@ -6,16 +6,16 @@ import pic3 from './image3.jpg';
 import './Homepage.css';
 
 const Homepage = () => {
-  const images = [pic1, pic2, pic3];
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  // const images = [pic1, pic2, pic3];
+  // const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 3);
-    }, 2000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 3);
+  //   }, 2000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
   // const Texts = [" surplus into sustenance"," waste into welfare","hunger into hope"];
   // const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
@@ -32,10 +32,6 @@ const Homepage = () => {
         <h1>Welcome Enthusiasts</h1>
         <div>Let's Turn <Example/> </div>
       </div>
-        <div class="slider-frame">
-              <img src={images[currentImageIndex]} className='img-container' alt="" />
-      
-        </div>
 
     </div>
   );
