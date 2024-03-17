d9f9ceb3a6ede2a74bb2c6830fc66878ad781e37
shrihari-joshi <joshishrihari13@gmail.com>
2024-03-17 08:54:29 +0530

    css

--- app/src/Components/Distributors.jsx
+++ app/src/Components/Distributors.jsx
@@ -1,38 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Distributors.css';
 
 const Distributors = () => {
   return (
-  <main>
-
-    <div>
-      <p>Our Distributors</p>
-      <p>Amidst the challenges faced by many, our platform stands as a beacon of hope and generosity. We extend our deepest appreciation to the restaurants, caterers, and grocery stores whose unwavering support and donations have fueled our mission of providing nourishment to those in need. Through their selfless contributions, countless individuals and families have found relief and sustenance during difficult times. Their kindness and dedication inspire us to continue our efforts in fostering stronger, more resilient communities. Together, we are making a tangible difference in the lives of many, one meal at a time.</p>
-    </div>
-
-    <div>
+    <main className="maincol">
       <div>
-        <p>NGO1</p>
+        <p className='distri'>Our Distributors</p>
+        <p className='dcontent'>Amidst the challenges faced by many, our platform stands as a beacon of hope and generosity. We extend our deepest appreciation to the restaurants, caterers, and grocery stores whose unwavering support and donations have fueled our mission of providing nourishment to those in need. Through their selfless contributions, countless individuals and families have found relief and sustenance during difficult times. Their kindness and dedication inspire us to continue our efforts in fostering stronger, more resilient communities. Together, we are making a tangible difference in the lives of many, one meal at a time.</p>
       </div>
 
-      <div>
-        <p>NGO2</p>
-      </div>
+      <div className="ngocontainer">
+        <div>
+          <p>NGO1</p>
+        </div>
 
-      <div>
-        <p>NGO3</p>
-      </div>
+        <div>
+          <p>NGO2</p>
+        </div>
 
-      <div>
-        <p>NGO4</p>
-      </div>
+        <div>
+          <p>NGO3</p>
+        </div>
 
-      <div>
-        <p>NGO5</p>
-      </div>
-      
-    </div>
-  </main> 
-  )
-}
+        <div>
+          <p>NGO4</p>
+        </div>
 
-export default Distributors
+        <div>
+          <p>NGO5</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Distributors;
